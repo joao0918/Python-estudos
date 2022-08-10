9b7481e1bb8e41f993173d864dbd7bc9db0d43ba
joao0918 <77458573+joao0918@users.noreply.github.com>
2022-08-10 12:54:30 -0300

    Create Exe-seção1.py

---
+++ Execícios/Exe-seção1.py
@@ -0,0 +1,28 @@
+"""
+01
+numero = 0
+numero = int(input("digite um numero inteiro: "))
+print(f"Numero digitado {numero}")
+
+02
+NumReal = float(input("Digite um numero real: "))
+print(f"Numero real recebido foi --> {NumReal}")
+
+03
+print("Digite 3 números inteiros")
+Enter1 = int(input("NUMBER 1 ="))
+Enter2 = int(input("NUMBER 2 ="))
+Enter3 = int(input("NUMBER 3 ="))
+
+soma = Enter1 + Enter2 + Enter3
+print(f"Soma dos ter números é igual a {soma}")
+
+04
+entrada = float(input("Numero real: "))
+quadrado = entrada ** 2
+print(f"Numero ao quadrado =  { quadrado:.2f}")
+
+05
+quinta = float(input("Numero: "))
+div = quinta/5
+print(f"a quinta parte {quinta} de é {div:.1f}")"""
