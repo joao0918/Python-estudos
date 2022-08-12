befdd0cf3439c59a7eadd7104af7cba3fae4f505
joao0918 <77458573+joao0918@users.noreply.github.com>
2022-08-12 03:07:16 -0300

    18.08.2022

--- Execícios/Exe-seção3.py
+++
@@ -1,7 +0,0 @@
-#nessa seção temos loop for, range, loop while com break
-
-'01'
-lista = []
-for multiplos in range(0,16,3):
-    lista.insert(multiplos,multiplos)   
-print(f" todos os multiplos de 3 = {lista}")    
